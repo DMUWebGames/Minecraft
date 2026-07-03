168a78e7219fba60327a263ddf420ed61b37dff0
Mohamed Djoncounda Sissoko <mohameddjoncoundasissoko@gmail.com>
2026-07-03 09:45:06 +0100

    Corrige the textures problem on Github

--- js/textures.js
+++ js/textures.js
@@ -12,10 +12,10 @@ function loadImage(src){
 
 export async function loadAllTextures() {
     const [dirt, stone, wood, water] = await Promise.all([
-        loadImage('../textures/dirt.png'),
-        loadImage('../textures/stone.png'),
-        loadImage('../textures/wood.png'),
-        loadImage('../textures/water.png'),
+        loadImage('textures/dirt.png'),
+        loadImage('textures/stone.png'),
+        loadImage('textures/wood.png'),
+        loadImage('textures/water.png'),
     ]);
     return { dirt, stone, wood, water };
 }
